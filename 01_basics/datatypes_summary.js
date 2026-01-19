67dd443a235ae0674858e4e0e1a9e33cac7d4b74
Prabin Sah <161099403+Prabin-09@users.noreply.github.com>
2026-01-19 20:06:14 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// Primitive
+// 7 types : String, Number, boolean, null, undefined, Symbol, Bigint
+
+// Statically typed or dymamically typed
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+// null returns object datatype
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 234567898765432n
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, function
+// datatypes objects....
+const heros = ["shaktiman", "laal", "dhaga"];
+let myObj = {
+    name: "karan",
+    age: 27
+}
+
+const myFunction = function (){
+    console.log("Hello world");
+}
+console.log(typeof myFunction)
+// function object
+
+// console.log(typeof bigNumber)
+// console.log(typeof outsideTemp);
+// https://262.ecma-international.org/5.1//#sec-11.4.3
