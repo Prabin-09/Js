9eee6093b01f4e59a83abcad62b6c2e3f9a24e85
Prabin Sah <161099403+Prabin-09@users.noreply.github.com>
2026-01-20 18:27:58 +0000

    Maths done

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -16,4 +16,20 @@ const hundreds = 1000000
 
 // ************** Maths *******************//
 
-console.log(Math);
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.min(4, 3, 5, 7));
+// console.log(Math.max(4, 3, 5, 7));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+
+// IMPORTANT
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
