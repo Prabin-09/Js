f2cc46dad4a1fb940c4129380ed5338e886026be
Prabin Sah <161099403+Prabin-09@users.noreply.github.com>
2026-01-23 18:20:14 +0000

    function remaining

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -24,7 +24,7 @@ const result = addTwoNumbers(3, 8)
 
 // console.log("Result:", result);
 
-function loginUserMessage(username){
+function loginUserMessage(username = "sam"){
     if(username === undefined){
         console.log("Please enter the username")
         return    
@@ -32,14 +32,38 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
+    // if(!username){
+    //     console.log("Please enter the username")
+    //     return    
+    // }
 // console.log(loginUserMessage("prabin"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("samm"));
 
+function calculateCartPrice(val1,val2, ...num1){// ... is a rest operator
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500, 2000))
 
+const user ={
+    username: "prabin",
+    price: 199
+}
 
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
 
+const myNewArray = [200, 400, 100, 600]
 
+function returnSecondValue(getArray){
+    return getArray[1]
+}
 
-
-
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 400, 1000]));
 
