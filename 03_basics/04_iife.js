c32a5fd24083aa7688f89044778a386098c4246d
Prabin Sah <161099403+Prabin-09@users.noreply.github.com>
2026-01-26 13:40:09 +0000

    iife immediately invoked function expression

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,29 @@
+// Immediately Invoked Function Expression(IIFE)
+(
+function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+}) ();
+
+// () function definition () execution
+( (name) =>  {
+    console.log(`DB CONNECTED TWO ${name}`);
+}) ('Sam')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
